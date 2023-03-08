e29234f871748b588e2548dfb37e20b100b0a467
Eternal-TMI <121382166+Eternal-TMI@users.noreply.github.com>
2023-03-08 17:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def show_results(posts, start, end , sss):
         return 0 , 0
     else :
         reply = sss
-        buttons = [[InlineKeyboardButton("HOW TO WATCH/DOWNLOAD❔",url=howtodownload)],[InlineKeyboardButton("Join Channel",url=channelurl),InlineKeyboardButton("🖥 Visit Web",url=web_domain)]]
+        buttons = [[InlineKeyboardButton("HOW TO WATCH/DOWNLOAD❓",url=howtodownload)],[InlineKeyboardButton("Join Channel",url=channelurl),InlineKeyboardButton("🖥 Visit Web",url=web_domain)]]
         # print(posts)
         for result in results:
             x = [InlineKeyboardButton(result['title'],url=result['url'])]
