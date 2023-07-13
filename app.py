a44d594b988b8f4d990dd4a2585590b65242ae1d
Eternal-TMI <121382166+Eternal-TMI@users.noreply.github.com>
2023-07-13 18:24:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 #Configssss -- Edit Alll
 API_ID = 6459362
 API_HASH = 'd7877fa235f24635921e287aaa800507'
-BOT_TOKEN = '5618692983:AAEbB2UfGVCnH-AUN-m-dQcfvgI7Dj4Br80'
+BOT_TOKEN = '5618692983:AAHR2eVn35QrYtLh4LExuhu-QguDPyNE7cA'
 TDMB_API = "b93049a713559ad90b95537da68308fe"
 web_domain = "https://www.xdubteam.in/"
 howtodownload = "https://youtu.be/-_WfkuVyJL4"
