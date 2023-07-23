444e9a4a3f1a31cd3f43f1bd9931b28a9424bb45
Eternal-TMI <121382166+Eternal-TMI@users.noreply.github.com>
2023-07-24 02:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ API_ID = 6459362
 API_HASH = 'd7877fa235f24635921e287aaa800507'
 BOT_TOKEN = '5618692983:AAHR2eVn35QrYtLh4LExuhu-QguDPyNE7cA'
 TDMB_API = "b93049a713559ad90b95537da68308fe"
-web_domain = "https://www.xdubteam.in/"
-howtodownload = "https://youtu.be/-_WfkuVyJL4"
+web_domain = "https://www.toonmixindia.in/"
+howtodownload = "https://www.toonmixindia.in/how-to-use-me.html"
 channelurl = "https://t.me/toonmix_india"
 
 
